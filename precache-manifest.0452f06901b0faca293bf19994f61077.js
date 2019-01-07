5ab49b5b733f5f7ccec87fca30e5c7fc65a28275
Julien Maupetit <julien@maupetit.net>
2019-01-08 00:00:22 +0100

    Updates

---
+++ precache-manifest.0452f06901b0faca293bf19994f61077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2d26659da1ddadf4108",
+    "url": "/buzzr/static/css/main.81eeec97.chunk.css"
+  },
+  {
+    "revision": "b2d26659da1ddadf4108",
+    "url": "/buzzr/static/js/main.b2d26659.chunk.js"
+  },
+  {
+    "revision": "61b63230e5b0e76c90bd",
+    "url": "/buzzr/static/js/1.61b63230.chunk.js"
+  },
+  {
+    "revision": "00837ccdcd50b6f75122",
+    "url": "/buzzr/static/js/runtime~main.00837ccd.js"
+  },
+  {
+    "revision": "2117e8e6c77d95d7483d247d0697cb83",
+    "url": "/buzzr/static/media/icon-cog.2117e8e6.svg"
+  },
+  {
+    "revision": "7ac620678a53938fd4e91811906aaa40",
+    "url": "/buzzr/index.html"
+  }
+];
